e8a543d098fcfe347027deaf2b030b8c8add5d8d
wonho1401 <wonho1401@naver.com>
2024-02-06 23:40:25 +0900

    feat: Add app bar with setting icon

--- app/lib/views/profile/profile_screen.dart
+++ app/lib/views/profile/profile_screen.dart
@@ -1,10 +1,30 @@
 import 'package:earth_and_i/utilities/system/font_system.dart';
 import 'package:earth_and_i/view_models/profile/profile_view_model.dart';
 import 'package:earth_and_i/views/base/base_screen.dart';
+import 'package:earth_and_i/widgets/appbar/custom_icon_button.dart';
+import 'package:earth_and_i/widgets/appbar/default_appbar.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends BaseScreen<ProfileViewModel> {
   const ProfileScreen({super.key});
+
+  @override
+  PreferredSizeWidget? buildAppBar(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(56),
+      child: DefaultAppBar(
+        title: "",
+        actions: [
+          CustomIconButton(
+            assetPath: "assets/icons/setting.svg",
+            onPressed: () {
+              debugPrint("plus button pressed");
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget buildBody(BuildContext context) {
