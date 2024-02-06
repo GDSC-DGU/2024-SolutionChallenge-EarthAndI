3b8093901b78711767f7eb2d27bb8a03ad877f14
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-02-07 03:00:51 +0900

    feat: Create Top Carbon Capacity Text

--- app/lib/view_models/home/home_view_model.dart
+++ app/lib/view_models/home/home_view_model.dart
@@ -1,3 +1,14 @@
 import 'package:get/get.dart';
 
-class HomeViewModel extends GetxController {}
+class HomeViewModel extends GetxController {
+  late final RxInt _reducedCO2;
+
+  int get reducedCO2 => _reducedCO2.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    _reducedCO2 = 12000.obs;
+  }
+}
--- app/lib/views/home/home_screen.dart
+++ app/lib/views/home/home_screen.dart
@@ -1,21 +1,47 @@
 import 'package:earth_and_i/utilities/system/font_system.dart';
 import 'package:earth_and_i/view_models/home/home_view_model.dart';
 import 'package:earth_and_i/views/base/base_screen.dart';
+import 'package:earth_and_i/views/home/widgets/character_layer.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends BaseScreen<HomeViewModel> {
   const HomeScreen({super.key});
 
   @override
-  Widget buildBody(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Text(
-          'Home',
-          style: FontSystem.KR18R,
+  PreferredSizeWidget? buildAppBar(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(92.0),
+      child: Container(
+        padding: const EdgeInsets.only(left: 16.0),
+        height: 92.0,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            '${NumberFormat('###,###,###,###').format(viewModel.reducedCO2)} kg',
+            style: FontSystem.KR24B,
+          ),
         ),
       ),
     );
   }
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Home Screen',
+        style: FontSystem.KR24B,
+      ),
+    );
+  }
+
+  @override
+  bool get wrapWithOuterSafeArea => true;
+
+  @override
+  bool get setTopOuterSafeArea => true;
+
+  @override
+  bool get setBottomOuterSafeArea => false;
 }
