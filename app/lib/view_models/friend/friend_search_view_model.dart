7366b37ce62617c45ef95b093e78cccea251819d
wonho1401 <wonho1401@naver.com>
2024-02-11 17:00:16 +0900

    refactor: modify searchController

--- app/lib/view_models/friend/friend_search_view_model.dart
+++ app/lib/view_models/friend/friend_search_view_model.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class FriendSearchViewModel extends GetxController {
-  final TextEditingController searchController = TextEditingController();
-  final RxString _searchWord = "".obs;
+  late final TextEditingController searchController;
+  late final RxString _searchWord = "".obs;
   late final _isKeyboardOpen = false.obs;
 
   String get searchWord => _searchWord.value;
   bool get isKeyboardOpen => _isKeyboardOpen.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    searchController = TextEditingController();
+  }
 
   void searchFriend(String value) {
     _searchWord.value = value;
@@ -19,5 +25,11 @@ class FriendSearchViewModel extends GetxController {
 
   void onPressReset() {
     _searchWord.value = "";
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
   }
 }
