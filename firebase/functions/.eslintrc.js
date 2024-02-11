32d51b3565f99616c5cadf13a505317a221a568c
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-02-11 18:56:09 +0900

    chore: Edit EsLint

--- firebase/functions/.eslintrc.js
+++ firebase/functions/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
+    "require-jsdoc": 0,
     "quotes": ["error", "double", {"allowTemplateLiterals": true}],
   },
   overrides: [
