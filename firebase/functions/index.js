999119314a80b4598427a57678fa9dc9ecf8cc85
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-02-11 19:08:23 +0900

    feat: add default value in RDB

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -4,16 +4,22 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.createUser = functions.auth.user().onCreate((user) => {
-  const displayName = user.displayName || "Anonymous";
+  const nickname = user.displayName || "Anonymous";
   const uid = user.uid;
 
   return createUser(displayName, uid);
 });
 
-async function createUser(displayName, uid) {
+async function createUser(nickname, uid) {
   await admin.database().ref(`/users/${uid}`).set({
-    displayName: displayName,
-    uid: uid,
+    nickname: nickname,
+    health_positive_cnt: 0,
+    health_negative_cnt: 0,
+    money_positive_cnt: 0,
+    money_negative_cnt: 0,
+    mental_positive_cnt: 0,
+    mental_negative_cnt: 0,
+    total_carbon_dioxide: 0.0,
   });
 
   functions.logger.log("User created: ", uid);
