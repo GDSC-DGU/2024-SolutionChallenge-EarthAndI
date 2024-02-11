a616a5a4f9c7053699ffbe95fe9fd06db84c91e2
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-02-11 21:22:26 +0900

    fix: Merge conflicts AndroidManifest.xml, Podfile.lock, Info.plist

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,12 +4,18 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
+  // DateTime Formatting
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initializeDateFormatting();
+
+  // Permission
+  await Permission.activityRecognition.request();
+
+  // Database
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MainApp());
 }
--- app/lib/view_models/root/root_view_model.dart
+++ app/lib/view_models/root/root_view_model.dart
@@ -1,5 +1,7 @@
+import 'package:earth_and_i/utilities/functions/dev_on_log.dart';
 import 'package:get/get.dart';
 import 'package:flutter/foundation.dart' as foundation;
+import 'package:health/health.dart';
 
 class RootViewModel extends GetxController {
   // Animation duration
@@ -19,13 +21,14 @@ class RootViewModel extends GetxController {
     super.onInit();
 
     isAndroid =
-        (foundation.defaultTargetPlatform == foundation.TargetPlatform.iOS);
+        (foundation.defaultTargetPlatform == foundation.TargetPlatform.android);
+    DevOnLog.d('isAndroid: $isAndroid, ${foundation.defaultTargetPlatform}');
 
     _selectedIndex = 1.obs;
     _isEnableGreyBarrier = false.obs;
   }
 
-  void changeIndex(int index) {
+  void changeIndex(int index) async {
     _selectedIndex.value = index;
   }
 
