e196cb33155d4b162c4ee0c3204b6a44461258c6
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-02-11 21:17:39 +0900

    fix: Merge conflicts pubspec, Podfile

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:earth_and_i/apps/firebase/firebase_options.dart';
 import 'package:earth_and_i/apps/main_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -6,6 +8,7 @@ import 'package:intl/date_symbol_data_local.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initializeDateFormatting();
 
   runApp(const MainApp());
