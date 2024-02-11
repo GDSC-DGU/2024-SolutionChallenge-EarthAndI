177e84da2e59d3c8830abff69103177b0f359bf8
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-02-11 18:56:21 +0900

    feat: Create User Trigger

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,19 +1,19 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions/v1");
+const admin = require("firebase-admin");
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+exports.createUser = functions.auth.user().onCreate((user) => {
+  const displayName = user.displayName || "Anonymous";
+  const uid = user.uid;
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+  return createUser(displayName, uid);
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+async function createUser(displayName, uid) {
+  await admin.database().ref(`/users/${uid}`).set({
+    displayName: displayName,
+    uid: uid,
+  });
+
+  functions.logger.log("User created: ", uid);
+  return null;
+}
