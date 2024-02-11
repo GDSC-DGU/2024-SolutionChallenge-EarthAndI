fbf5916ff2e28483373c19a0ebda83aa2bf5a12f
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2024-02-12 03:42:52 +0900

    :sparkles: Feature - Language add Section 언어들

--- app/lib/views/language/language_screen.dart
+++ app/lib/views/language/language_screen.dart
@@ -1,8 +1,11 @@
+import 'package:earth_and_i/utilities/system/color_system.dart';
 import 'package:earth_and_i/utilities/system/font_system.dart';
 import 'package:earth_and_i/view_models/language/language_view_model.dart';
 import 'package:earth_and_i/views/base/base_screen.dart';
 import 'package:earth_and_i/widgets/appbar/default_back_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
 class LanguageScreen extends BaseScreen<LanguageViewModel> {
   const LanguageScreen({super.key});
@@ -19,14 +22,43 @@ class LanguageScreen extends BaseScreen<LanguageViewModel> {
 
   @override
   Widget buildBody(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Text(
-          'Language',
-          style: FontSystem.KR18R,
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          languageSection("영어"),
+          languageSection("한국어"),
+        ],
       ),
     );
   }
+
+  Widget languageSection(
+    String language,
+  ) =>
+      Container(
+        decoration: const BoxDecoration(
+          border:
+              Border(bottom: BorderSide(width: 1, color: ColorSystem.grey1)),
+        ),
+        width: double.infinity,
+        child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    language,
+                    style: FontSystem.KR16B,
+                  ),
+                ),
+                if (language == "한국어")
+                  SvgPicture.asset(
+                    'assets/icons/check.svg',
+                    width: 20,
+                  )
+              ],
+            )),
+      );
 }
