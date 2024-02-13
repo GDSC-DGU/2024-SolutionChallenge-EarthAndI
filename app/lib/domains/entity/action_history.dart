88f149d808f38c5b8603a40e7c5827026a39ea82
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2024-02-14 00:08:58 +0900

    comment: Insert Table Comment

--- app/lib/domains/entity/action_history.dart
+++ app/lib/domains/entity/action_history.dart
@@ -2,6 +2,15 @@ import 'package:drift/drift.dart';
 import 'package:earth_and_i/domains/type/e_action.dart';
 import 'package:earth_and_i/domains/type/e_user_status.dart';
 
+/// ActionHistory Table
+/// 1. id: Auto Increment ID
+/// 2. createdAt: Created At
+/// 3. updatedAt: Updated At
+/// 4. userStatus: User Status(Health, Mental, Cash)
+/// 5. type: Action Type(EAction)
+/// 6. question: Question
+/// 7. answer: Answer
+/// 8. changeCapacity: Change Capacity
 class ActionHistory extends Table {
   IntColumn get id => integer().autoIncrement()();
 
--- app/lib/domains/entity/challenge_history.dart
+++ app/lib/domains/entity/challenge_history.dart
@@ -2,6 +2,14 @@ import 'package:drift/drift.dart';
 import 'package:earth_and_i/domains/type/e_challenge.dart';
 import 'package:earth_and_i/domains/type/e_user_status.dart';
 
+/// ChallengeHistory Table
+/// 1. id: Auto Increment ID
+/// 2. createdAt: Created At
+/// 3. updatedAt: Updated At
+/// 4. userStatus: User Status(Health, Mental, Cash)
+/// 5. type: Challenge Type(EChallenge)
+/// 6. analysisContent: Analysis Content
+/// 7. changeCapacity: Change Capacity
 class ChallengeHistory extends Table {
   IntColumn get id => integer().autoIncrement()();
 
