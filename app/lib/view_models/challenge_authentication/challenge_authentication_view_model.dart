c21d014adf91bd00ba676e7ccd27ecc2d3122adf
wonho1401 <wonho1401@naver.com>
2024-02-14 00:12:49 +0900

    fix: Add onInit, dispose

--- app/lib/view_models/challenge_authentication/challenge_authentication_view_model.dart
+++ app/lib/view_models/challenge_authentication/challenge_authentication_view_model.dart
@@ -2,8 +2,15 @@ import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ChallengeAuthenticationViewModel extends GetxController {
-  final _image = Rx<XFile?>(null);
+  late final Rxn<XFile?> _image;
+
   XFile? get image => _image.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _image = Rxn<XFile?>();
+  }
 
   void getImage() async {
     final XFile? image =
@@ -15,5 +22,11 @@ class ChallengeAuthenticationViewModel extends GetxController {
 
   void removeImage() {
     _image.value = null;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _image.close();
   }
 }
