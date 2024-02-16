da79faa2ef56da95c007a8a5400c46fefbe0060e
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2024-02-16 23:16:35 +0900

    :sparkles: Feat : Alram toggle

--- app/lib/view_models/setting/setting_view_model.dart
+++ app/lib/view_models/setting/setting_view_model.dart
@@ -1,3 +1,27 @@
 import 'package:get/get.dart';
 
-class SettingViewModel extends GetxController {}
+class SettingViewModel extends GetxController {
+  late final RxBool _isAlram = true.obs;
+  late final RxString _alramTime = "09:00".obs;
+
+  bool get isAlram => _isAlram.value;
+  String get alramTime => _alramTime.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  void onIsAlramSwitch() {
+    _isAlram.value = !_isAlram.value;
+  }
+
+  void onAlramTimeSet() {
+    print("클릭");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
--- app/lib/views/setting/setting_screen.dart
+++ app/lib/views/setting/setting_screen.dart
@@ -5,6 +5,7 @@ import 'package:earth_and_i/view_models/setting/setting_view_model.dart';
 import 'package:earth_and_i/views/base/base_screen.dart';
 import 'package:earth_and_i/widgets/appbar/default_back_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
 class SettingScreen extends BaseScreen<SettingViewModel> {
@@ -29,6 +30,7 @@ class SettingScreen extends BaseScreen<SettingViewModel> {
         children: [
           settingSection("사용자 인증", Routes.USERAUTH),
           settingSection("언어", Routes.LANGUAGE),
+          Obx(() => settingAlram()),
         ],
       ),
     );
@@ -52,4 +54,105 @@ class SettingScreen extends BaseScreen<SettingViewModel> {
               )),
         ),
       );
+
+  Widget settingAlram() => Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          //알림 활성화
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "알림 활성화",
+                      style: FontSystem.KR16B,
+                    ),
+
+                    //---토글버튼
+                    toggleButton(viewModel.isAlram, viewModel.onIsAlramSwitch)
+                  ],
+                )),
+          ),
+
+          const SizedBox(
+            height: 8,
+          ),
+
+          //알림 시간
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("알림 시간",
+                        style: FontSystem.KR16B.copyWith(
+                            color: viewModel.isAlram
+                                ? Colors.black
+                                : const Color(0xFFACADB2))),
+
+                    //--- 시간선택 버튼
+
+                    InkWell(
+                      onTap: () {
+                        viewModel.isAlram ? viewModel.onAlramTimeSet() : {};
+                      },
+                      child: Row(
+                        children: [
+                          Text(viewModel.alramTime,
+                              style: FontSystem.KR16R.copyWith(
+                                  color: viewModel.isAlram
+                                      ? Colors.black
+                                      : const Color(0xFFACADB2))),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          SvgPicture.asset(
+                            'assets/icons/right.svg',
+                            width: 16,
+                            colorFilter: ColorFilter.mode(
+                                viewModel.isAlram
+                                    ? Colors.black
+                                    : const Color(0xFFACADB2),
+                                BlendMode.srcATop),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+          ),
+        ],
+      );
 }
+
+Widget toggleButton(bool value, void Function() action) => InkWell(
+    onTap: action,
+    child: Container(
+      width: 44,
+      height: 22,
+      decoration: BoxDecoration(
+        color: value ? const Color(0xFF90CDBE) : const Color(0xFFACADB2),
+        borderRadius: BorderRadius.circular(22),
+      ),
+      child: Row(
+        mainAxisAlignment:
+            value ? MainAxisAlignment.start : MainAxisAlignment.end,
+        children: [
+          Container(
+            width: 22,
+            height: 22,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white, // 하얀 동그라미
+            ),
+          ),
+        ],
+      ),
+    ));
