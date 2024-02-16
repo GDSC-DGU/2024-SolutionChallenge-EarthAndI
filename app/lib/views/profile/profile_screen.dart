922f92e026a705aa0908e396975e4642383255ba
wonho1401 <wonho1401@naver.com>
2024-02-16 21:59:26 +0900

    feat: Modify Profile Screen

--- app/lib/views/profile/profile_screen.dart
+++ app/lib/views/profile/profile_screen.dart
@@ -1,15 +1,26 @@
 import 'package:earth_and_i/utilities/static/app_routes.dart';
-import 'package:earth_and_i/utilities/system/font_system.dart';
 import 'package:earth_and_i/view_models/profile/profile_view_model.dart';
 import 'package:earth_and_i/views/base/base_screen.dart';
+import 'package:earth_and_i/views/profile/widgets/user_history.dart';
 import 'package:earth_and_i/views/profile/widgets/user_profile.dart';
+import 'package:earth_and_i/views/profile/widgets/weekly_calendar.dart';
 import 'package:earth_and_i/widgets/appbar/custom_icon_button.dart';
 import 'package:earth_and_i/widgets/appbar/default_appbar.dart';
+import 'package:extended_nested_scroll_view/extended_nested_scroll_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfileScreen extends BaseScreen<ProfileViewModel> {
   const ProfileScreen({super.key});
+
+  @override
+  bool get wrapWithOuterSafeArea => true;
+
+  @override
+  bool get setTopOuterSafeArea => true;
+
+  @override
+  bool get setBottomOuterSafeArea => false;
 
   @override
   PreferredSizeWidget? buildAppBar(BuildContext context) {
@@ -31,51 +42,17 @@ class ProfileScreen extends BaseScreen<ProfileViewModel> {
 
   @override
   Widget buildBody(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const UserProfile(),
-        _tabBar(context),
-        _tabBarView(),
-      ],
+    return ExtendedNestedScrollView(
+      headerSliverBuilder: ((context, innerBoxIsScrolled) =>
+          [const SliverToBoxAdapter(child: UserProfile())]),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            WeeklyCalendar(),
+            UserHistory(),
+          ],
+        ),
+      ),
     );
   }
-
-  Widget _tabBar(BuildContext context) => SizedBox(
-        width: Get.width,
-        height: Get.width * 0.12,
-        child: TabBar(
-          controller: viewModel.tabController,
-          padding: const EdgeInsets.only(left: 16, right: 16),
-
-          indicatorColor: Colors.black,
-          indicatorSize: TabBarIndicatorSize.tab,
-
-          // 선택된 탭의 색상과 폰트 스타일
-          labelColor: Colors.black,
-          labelStyle: FontSystem.KR16SB,
-
-          // 선택되지 않은 탭의 색상과 폰트 스타일
-          unselectedLabelColor: const Color(0xFFACADB2),
-          unselectedLabelStyle: FontSystem.KR16SB,
-
-          // 탭 선택시 하이라이트 제거
-          overlayColor: MaterialStateProperty.all(Colors.transparent),
-
-          tabs: const [
-            Tab(text: "배지"),
-            Tab(text: "기록"),
-          ],
-        ),
-      );
-
-  Widget _tabBarView() => Expanded(
-        child: TabBarView(
-          controller: viewModel.tabController,
-          children: const [
-            Center(child: Text("Badge Screen")),
-            Center(child: Text("Record Screen")),
-          ],
-        ),
-      );
 }
