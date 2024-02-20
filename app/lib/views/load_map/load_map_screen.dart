5ddede7c1a0b3581f2b214b05c24f492a645d165
wonho1401 <wonho1401@naver.com>
2024-02-21 00:32:46 +0900

    refactor: Refactor code about challenges

--- app/lib/views/load_map/load_map_screen.dart
+++ app/lib/views/load_map/load_map_screen.dart
@@ -41,67 +41,76 @@ class LoadMapScreen extends BaseScreen<LoadMapViewModel> {
 
             const SizedBox(height: 24),
             // 현재 진행중인 챌린지 (한 개만 표시)
-            const Text("진행중인 챌린지", style: FontSystem.KR20SB120),
-            const SizedBox(height: 16),
-            SizedBox(
-              height: 110,
-              child: Obx(
-                () => ListView.builder(
-                  itemCount: viewModel.currentChallengeHistoryState.length,
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemBuilder: (BuildContext context, index) {
-                    return ChallengeList(
-                        challenge:
-                            viewModel.currentChallengeHistoryState[index],
-                        isCompleted: viewModel
-                            .currentChallengeHistoryState[index].isCompleted);
-                  },
-                ),
-              ),
-            ),
+            currentChallengeView(),
 
             Divider(thickness: 2, color: ColorSystem.grey[100]!),
             const SizedBox(height: 20),
             // 완료한 챌린지 헤더
-            Row(
-              children: [
-                const Text("완료한 챌린지", style: FontSystem.KR20SB120),
-                const SizedBox(width: 8),
-                Obx(
-                  () => Text(
-                      "${viewModel.completedChallengeHistoryState.length}",
-                      style: FontSystem.KR20SB120
-                          .copyWith(color: ColorSystem.grey[500]!)),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 16),
-            // 완료한 챌린지 리스트 뷰 (여러 개 표시)
-            Obx(
-              () => ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: viewModel.completedChallengeHistoryState.length,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, index) {
-                  return Column(
-                    children: [
-                      ChallengeList(
-                          challenge:
-                              viewModel.completedChallengeHistoryState[index],
-                          isCompleted: viewModel
-                              .completedChallengeHistoryState[index]
-                              .isCompleted),
-                      const SizedBox(height: 12),
-                    ],
-                  );
-                },
-              ),
-            ),
+            completedChallengeView(),
           ],
         ),
       ),
     );
   }
+
+  Widget currentChallengeView() => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("진행중인 챌린지", style: FontSystem.KR20SB120),
+          const SizedBox(height: 16),
+          SizedBox(
+            height: 110,
+            child: Obx(
+              () => ListView.builder(
+                itemCount: viewModel.currentChallengeHistoryState.length,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, index) {
+                  return ChallengeList(
+                      challenge: viewModel.currentChallengeHistoryState[index],
+                      isCompleted: viewModel
+                          .currentChallengeHistoryState[index].isCompleted);
+                },
+              ),
+            ),
+          ),
+        ],
+      );
+
+  Widget completedChallengeView() => Column(
+        children: [
+          Row(
+            children: [
+              const Text("완료한 챌린지", style: FontSystem.KR20SB120),
+              const SizedBox(width: 8),
+              Obx(
+                () => Text("${viewModel.completedChallengeHistoryState.length}",
+                    style: FontSystem.KR20SB120
+                        .copyWith(color: ColorSystem.grey[500]!)),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Obx(
+            () => ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: viewModel.completedChallengeHistoryState.length,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, index) {
+                return Column(
+                  children: [
+                    ChallengeList(
+                        challenge:
+                            viewModel.completedChallengeHistoryState[index],
+                        isCompleted: viewModel
+                            .completedChallengeHistoryState[index].isCompleted),
+                    const SizedBox(height: 12),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      );
 }
