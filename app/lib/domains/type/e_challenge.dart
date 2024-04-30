3ee167285b8a2a424f53e2f15eaad0bcb95ba3ea
wonho1401 <wonho1401@naver.com>
2024-05-01 04:18:46 +0900

    feat: Add Coming Soon Challenge List

--- app/lib/domains/type/e_challenge.dart
+++ app/lib/domains/type/e_challenge.dart
@@ -3,6 +3,15 @@ enum EChallenge implements Comparable<EChallenge> {
   deleteEmail,
   eatVegetarian,
   useColdWater,
+  usePublicTransport,
+  useReusableContainer,
+  unplugDevices,
+  rideBike,
+  buyLocalProducts,
+  recycle,
+  useLED,
+  useEnergyEfficiency,
+  climateControl,
   ;
 
   const EChallenge();
@@ -23,6 +32,24 @@ enum EChallenge implements Comparable<EChallenge> {
         return 'use_cold_water.png';
       case EChallenge.eatVegetarian:
         return 'eat_vegetarian.png';
+      case EChallenge.usePublicTransport:
+        return 'use_public_transport.png';
+      case EChallenge.useReusableContainer:
+        return 'use_reusable_container.png';
+      case EChallenge.unplugDevices:
+        return 'unplug_devices.png';
+      case EChallenge.rideBike:
+        return 'ride_bike.png';
+      case EChallenge.buyLocalProducts:
+        return 'buy_local_products.png';
+      case EChallenge.recycle:
+        return 'recycle.png';
+      case EChallenge.useLED:
+        return 'use_led.png';
+      case EChallenge.useEnergyEfficiency:
+        return 'use_energy_efficiency.png';
+      case EChallenge.climateControl:
+        return 'climate_control.png';
       default:
         throw Exception('Unknown EChallenge: $this');
     }
@@ -37,6 +64,24 @@ enum EChallenge implements Comparable<EChallenge> {
       case EChallenge.useColdWater:
         return '${name}_short_title';
       case EChallenge.eatVegetarian:
+        return '${name}_short_title';
+      case EChallenge.usePublicTransport:
+        return '${name}_short_title';
+      case EChallenge.useReusableContainer:
+        return '${name}_short_title';
+      case EChallenge.unplugDevices:
+        return '${name}_short_title';
+      case EChallenge.rideBike:
+        return '${name}_short_title';
+      case EChallenge.buyLocalProducts:
+        return '${name}_short_title';
+      case EChallenge.recycle:
+        return '${name}_short_title';
+      case EChallenge.useLED:
+        return '${name}_short_title';
+      case EChallenge.useEnergyEfficiency:
+        return '${name}_short_title';
+      case EChallenge.climateControl:
         return '${name}_short_title';
       default:
         throw Exception('Unknown EChallenge: $this');
@@ -53,6 +98,24 @@ enum EChallenge implements Comparable<EChallenge> {
         return '${name}_long_title';
       case EChallenge.eatVegetarian:
         return '${name}_long_title';
+      case EChallenge.usePublicTransport:
+        return '${name}_long_title';
+      case EChallenge.useReusableContainer:
+        return '${name}_long_title';
+      case EChallenge.unplugDevices:
+        return '${name}_long_title';
+      case EChallenge.rideBike:
+        return '${name}_long_title';
+      case EChallenge.buyLocalProducts:
+        return '${name}_long_title';
+      case EChallenge.recycle:
+        return '${name}_long_title';
+      case EChallenge.useLED:
+        return '${name}_long_title';
+      case EChallenge.useEnergyEfficiency:
+        return '${name}_long_title';
+      case EChallenge.climateControl:
+        return '${name}_long_title';
       default:
         throw Exception('Unknown EChallenge: $this');
     }
@@ -67,6 +130,24 @@ enum EChallenge implements Comparable<EChallenge> {
       case EChallenge.useColdWater:
         return '${name}_description';
       case EChallenge.eatVegetarian:
+        return '${name}_description';
+      case EChallenge.usePublicTransport:
+        return '${name}_description';
+      case EChallenge.useReusableContainer:
+        return '${name}_description';
+      case EChallenge.unplugDevices:
+        return '${name}_description';
+      case EChallenge.rideBike:
+        return '${name}_description';
+      case EChallenge.buyLocalProducts:
+        return '${name}_description';
+      case EChallenge.recycle:
+        return '${name}_description';
+      case EChallenge.useLED:
+        return '${name}_description';
+      case EChallenge.useEnergyEfficiency:
+        return '${name}_description';
+      case EChallenge.climateControl:
         return '${name}_description';
       default:
         throw Exception('Unknown EChallenge: $this');
@@ -83,6 +164,24 @@ enum EChallenge implements Comparable<EChallenge> {
         return EChallenge.useColdWater;
       case 'eatVegetarian':
         return EChallenge.eatVegetarian;
+      case 'usePublicTransport':
+        return EChallenge.usePublicTransport;
+      case 'useReusableContainer':
+        return EChallenge.useReusableContainer;
+      case 'unplugDevices':
+        return EChallenge.unplugDevices;
+      case 'rideBike':
+        return EChallenge.rideBike;
+      case 'buyLocalProducts':
+        return EChallenge.buyLocalProducts;
+      case 'recycle':
+        return EChallenge.recycle;
+      case 'useLED':
+        return EChallenge.useLED;
+      case 'useEnergyEfficiency':
+        return EChallenge.useEnergyEfficiency;
+      case 'climateControl':
+        return EChallenge.climateControl;
       default:
         throw Exception('Unknown EChallenge: $name');
     }
---
+++ app/lib/views/coming_soon_challenge/static/coming_soon_challenge_states.dart
@@ -0,0 +1,43 @@
+import 'package:earth_and_i/domains/type/e_challenge.dart';
+import 'package:earth_and_i/models/load_map/challenge_history_state.dart';
+
+List<ChallengeHistoryState> getComingSoonChallengeStates() {
+  return [
+    ChallengeHistoryState(
+      type: EChallenge.usePublicTransport,
+      isCompleted: false,
+    ),
+    ChallengeHistoryState(
+      type: EChallenge.useReusableContainer,
+      isCompleted: false,
+    ),
+    ChallengeHistoryState(
+      type: EChallenge.unplugDevices,
+      isCompleted: false,
+    ),
+    ChallengeHistoryState(
+      type: EChallenge.rideBike,
+      isCompleted: false,
+    ),
+    ChallengeHistoryState(
+      type: EChallenge.buyLocalProducts,
+      isCompleted: false,
+    ),
+    ChallengeHistoryState(
+      type: EChallenge.recycle,
+      isCompleted: false,
+    ),
+    ChallengeHistoryState(
+      type: EChallenge.useLED,
+      isCompleted: false,
+    ),
+    ChallengeHistoryState(
+      type: EChallenge.useEnergyEfficiency,
+      isCompleted: false,
+    ),
+    ChallengeHistoryState(
+      type: EChallenge.climateControl,
+      isCompleted: false,
+    )
+  ];
+}
