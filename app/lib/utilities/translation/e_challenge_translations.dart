25faa87e43cf61dd48f67ee625083c5f68b4a38b
wonho1401 <wonho1401@naver.com>
2024-05-02 00:50:03 +0900

    feat: Add Coming Soon Challenge Translations for English

--- app/lib/utilities/translation/e_challenge_translations.dart
+++ app/lib/utilities/translation/e_challenge_translations.dart
@@ -96,7 +96,13 @@ abstract class EChallengeTranslations {
             '자동차 대신 대중교통을 이용하면 탄소 배출을 크게 줄일 수 있습니다. 하루 대중교통을 이용할 경우, 약 4.8kg의 이산화탄소 배출을 줄일 수 있습니다!',
       };
 
-  static Map<String, String> get enUsePublicTransport => {};
+  static Map<String, String> get enUsePublicTransport => {
+        'usePublicTransport_short_title': 'Use Public Transport',
+        'usePublicTransport_long_title':
+            'Show yourself using public transportation!',
+        'usePublicTransport_description':
+            'By using public transportation instead of a car, you can greatly reduce carbon emissions. If you use public transportation for a day, you can reduce carbon dioxide emissions by about 4.8kg!',
+      };
 
   static Map<String, String> get krUseReusableContainer => {
         'useReusableContainer_short_title': '재사용 가능한 용기 사용하기',
@@ -105,7 +111,13 @@ abstract class EChallengeTranslations {
             '일회용 제품 대신 재사용 가능한 용기를 사용하면 쓰레기양과 탄소 배출을 동시에 줄일 수 있습니다. 평균적으로 재사용 용기 하나를 사용하면, 약 0.5kg의 탄소가 절감됩니다!',
       };
 
-  static Map<String, String> get enUseReusableContainer => {};
+  static Map<String, String> get enUseReusableContainer => {
+        'useReusableContainer_short_title': 'Use Reusable Containers',
+        'useReusableContainer_long_title':
+            'Use reusable containers instead of disposable containers!',
+        'useReusableContainer_description':
+            'By using reusable containers instead of disposable products, you can reduce waste and carbon emissions at the same time. On average, using one reusable container saves about 0.5kg of carbon!',
+      };
 
   static Map<String, String> get krUnplugDevices => {
         'unplugDevices_short_title': '전기기기 플러그 뽑기',
@@ -114,7 +126,13 @@ abstract class EChallengeTranslations {
             '사용하지 않을 때 전자기기의 플러그를 뽑아 대기 전력을 차단하면, 연간 최대 50kg의 탄소를 감축할 수 있습니다!',
       };
 
-  static Map<String, String> get enUnplugDevices => {};
+  static Map<String, String> get enUnplugDevices => {
+        'unplugDevices_short_title': 'Unplug Electronic Devices',
+        'unplugDevices_long_title':
+            'Unplug the plug of unused electronic devices!',
+        'unplugDevices_description':
+            'When you unplug the plug of electronic devices when not in use to block standby power, you can reduce up to 50kg of carbon annually!',
+      };
 
   static Map<String, String> get krRideBike => {
         'rideBike_short_title': '자전거 타기',
@@ -123,7 +141,12 @@ abstract class EChallengeTranslations {
             '자전거를 이용하면 차량 사용을 줄여 탄소 배출을 감소시킬 수 있습니다. 하루 10km 자전거를 탈 경우, 약 1.3kg의 탄소 배출을 줄일 수 있습니다!',
       };
 
-  static Map<String, String> get enRideBike => {};
+  static Map<String, String> get enRideBike => {
+        'rideBike_short_title': 'Ride a Bike',
+        'rideBike_long_title': 'Share the moment you ride a bike!',
+        'rideBike_description':
+            'By using a bicycle, you can reduce carbon emissions by reducing vehicle use. If you ride a bicycle 10km a day, you can reduce carbon emissions by about 1.3kg!',
+      };
 
   static Map<String, String> get krBuyLocalProducts => {
         'buyLocalProducts_short_title': '지역 농산물 구매하기',
@@ -132,7 +155,13 @@ abstract class EChallengeTranslations {
             '지역 농산물을 구매하면 장거리 운송으로 인한 탄소 배출을 줄일 수 있습니다. 지역 농산물을 구매하면 이동 거리당 탄소 배출을 약 10% 감소시킬 수 있습니다!',
       };
 
-  static Map<String, String> get enBuyLocalProducts => {};
+  static Map<String, String> get enBuyLocalProducts => {
+        'buyLocalProducts_short_title': 'Buy Local Agricultural Products',
+        'buyLocalProducts_long_title':
+            'Consume local agricultural products to reduce food mileage!',
+        'buyLocalProducts_description':
+            'Purchasing local agricultural products can reduce carbon emissions due to long-distance transportation. By purchasing local agricultural products, you can reduce carbon emissions per distance by about 10%!',
+      };
 
   static Map<String, String> get krRecycle => {
         'recycle_short_title': '분리수거하기',
@@ -141,7 +170,12 @@ abstract class EChallengeTranslations {
             '폐기물을 적절하게 분리 수거함으로써 재활용 비율을 높이고, 자원의 재사용을 통해 탄소 배출을 줄일 수 있습니다. 재활용 한 품목당 약 0.2kg의 탄소가 절감됩니다!',
       };
 
-  static Map<String, String> get enRecycle => {};
+  static Map<String, String> get enRecycle => {
+        'recycle_short_title': 'Recycle',
+        'recycle_long_title': 'Please separate recyclable waste!',
+        'recycle_description':
+            'By properly separating and collecting waste, you can increase the recycling rate and reduce carbon emissions through the reuse of resources. About 0.2kg of carbon is saved per recycled item!',
+      };
 
   static Map<String, String> get krUseLED => {
         'useLED_short_title': 'LED 전구 사용하기',
@@ -150,7 +184,12 @@ abstract class EChallengeTranslations {
             'LED 전구는 전통적인 전구에 비해 훨씬 적은 에너지를 사용하며, 전구 하나를 LED로 교체만 해도 연간 약 40kg의 탄소 배출을 감소시킬 수 있습니다!',
       };
 
-  static Map<String, String> get enUseLED => {};
+  static Map<String, String> get enUseLED => {
+        'useLED_short_title': 'Use LED Bulbs',
+        'useLED_long_title': 'Show a house using LED bulbs!',
+        'useLED_description':
+            'LED bulbs use much less energy than traditional bulbs, and simply replacing one bulb with LED can reduce carbon emissions by about 40kg annually!',
+      };
 
   static Map<String, String> get krUseEnergyEfficiency => {
         'useEnergyEfficiency_short_title': '에너지 효율 확인하기',
@@ -159,7 +198,13 @@ abstract class EChallengeTranslations {
             '에너지 효율이 높은 가전 제품을 사용하면 에너지 소비를 줄이고, 그에 따른 탄소 배출도 감소시킬 수 있습니다. 평균적으로 에너지 효율이 높은 제품은 기존 제품 대비 약 20%의 에너지를 절감할 수 있습니다!',
       };
 
-  static Map<String, String> get enUseEnergyEfficiency => {};
+  static Map<String, String> get enUseEnergyEfficiency => {
+        'useEnergyEfficiency_short_title': 'Check Energy Efficiency',
+        'useEnergyEfficiency_long_title':
+            'Try using energy-efficient home appliances!',
+        'useEnergyEfficiency_description':
+            'Using energy-efficient home appliances can reduce energy consumption and reduce carbon emissions. On average, energy-efficient products can save about 20% of energy compared to existing products!',
+      };
 
   static Map<String, String> get krClimateControl => {
         'climateControl_short_title': '실내 온도 조절하기',
@@ -168,5 +213,11 @@ abstract class EChallengeTranslations {
             '실내 온도를 조절하여 에너지 사용을 최적화해 난방 및 냉방으로 인한 탄소 배출을 줄일 수 있습니다. 연간 최대 15%의 에너지 절감 효과가 있습니다!',
       };
 
-  static Map<String, String> get enClimateControl => {};
+  static Map<String, String> get enClimateControl => {
+        'climateControl_short_title': 'Adjust Indoor Temperature',
+        'climateControl_long_title':
+            'Adjust indoor temperature according to the season!',
+        'climateControl_description':
+            'By adjusting the indoor temperature to optimize energy use, you can reduce carbon emissions from heating and cooling. There is an annual maximum energy saving effect of 15%!',
+      };
 }
